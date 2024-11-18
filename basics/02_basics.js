5b8011ec195b33c8be515efc72deaa820ab6948d
Bhavy <bhavysarwa51@gmail.com>
2024-11-18 10:03:43 +0000

    data types

---
+++ basics/02_basics.js
@@ -0,0 +1,16 @@
+//treat all version as new js strict but not needed because by deafulr the changes are already considerd new 
+"use strict";
+ 
+//can not use directly in node , but can do it through the browser documents/devloper options
+//alert("hello")
+
+console.log(1.4+7.93)
+console.log("Bhavy")
+
+console.log(typeof "Bhavy");
+console.log(typeof 12.3456);
+console.log(typeof 12.65);
+console.log(typeof 123);
+console.log(typeof 'a');
+console.log(typeof true );
+console.log(typeof null);
