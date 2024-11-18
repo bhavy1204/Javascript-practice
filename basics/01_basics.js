590a4c09feb7d516299301b1b244809573ecb11a
Bhavy <bhavysarwa51@gmail.com>
2024-11-18 09:47:56 +0000

    basics

---
+++ basics/01_basics.js
@@ -0,0 +1,15 @@
+const accountId=14453;
+let accountEmail="nhibatunga"
+var accountPassword="12345"
+accountCity="jaipur"
+//not allowed
+// accountId=9876;
+
+accountEmail="batunga"
+accountPassword="2345"
+acoountcity="udaipur"
+console.log(accountId)
+
+let accountState
+//in a tabular form
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
